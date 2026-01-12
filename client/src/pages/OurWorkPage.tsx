d6928b6a9984f6b2c023cd24bfbc024e48b8c530
bottemiller1793 <52951230-bottemiller1793@users.noreply.replit.com>
2026-01-12 06:04:53 +0000

    Initial commit

---
+++ client/src/pages/OurWorkPage.tsx
@@ -0,0 +1,15 @@
+export const OurWorkPage = (): JSX.Element => {
+  return (
+    <div className="overflow-hidden w-full min-w-[1080px] h-[2059px] relative">
+      <img
+        className="absolute w-full h-[99.99%] top-[1012.77%] left-[173.06%]"
+        alt="Page"
+      />
+
+      <img
+        className="absolute w-full h-[99.99%] top-0 left-0"
+        alt="Clip path group"
+      />
+    </div>
+  );
+};
